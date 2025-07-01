d77395aea21357e03161168fac80eb226ca52821
Anish Sukhramani <91376836+AnishSukhramani@users.noreply.github.com>
2025-07-02 00:06:39 +0530

    file uploads ui components added

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import { Input } from "@/components/ui/input";
+import { Switch } from "@/components/ui/switch";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Neurality Health</h1>
+      <Switch id="airplane-mode" />
+      <br/>
+      <Input id="picture" type="file" />
     </div>
   );
 }
