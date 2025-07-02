61d5ed912cfc56aa4c6557d3248d18be748c53d3
Anish Sukhramani <91376836+AnishSukhramani@users.noreply.github.com>
2025-07-02 10:56:36 +0530

    implemented toggle to switch between filetypes
    Based on the toggle selection, now only csv or excel formats will be accepted files and other files will not be uploaded.

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,33 @@
+"use client"
+
 import Image from "next/image";
+import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Switch } from "@/components/ui/switch";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const [isCsv, setIsCsv] = useState(false);
+  const acceptedFileTypes = isCsv
+    ? ".csv"
+    : ".xls,.xlsx,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+  const handleToggleChange = (checked) => {
+    setIsCsv(checked);
+  };
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Neurality Health</h1>
-      <Switch id="airplane-mode" />
-      <br/>
-      <Input id="picture" type="file" />
+    <div className="space-y-4 p-4">
+      <h1 className="text-blue-500 font-bold text-xl">Neurality Health</h1>
+
+      <div className="flex items-center space-x-2">
+        <p>Excel</p>
+        <Switch id="file-toggle" checked={isCsv} onCheckedChange={handleToggleChange} />
+        <p>CSV</p>
+      </div>
+
+      <Input id="file-upload" type="file" accept={acceptedFileTypes} />
+
+      <Button>Upload</Button>
     </div>
   );
 }
