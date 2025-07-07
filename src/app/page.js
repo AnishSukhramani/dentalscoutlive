25bfaf558e7bae990ced4f0bbdbb51d5fd6edfa5
Anish Sukhramani <91376836+AnishSukhramani@users.noreply.github.com>
2025-07-07 22:03:20 +0530

    updated todo

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,7 @@
 "use client"
 //TODO: Integrate Google Sheets as an alternative to Excel/CSV
+//TODO: Modularize the code into components
+
 import Image from "next/image";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
