2b6b345a19f7488508d01f1fef5e92656ffbc26c
Anish Sukhramani <91376836+AnishSukhramani@users.noreply.github.com>
2025-07-22 09:06:40 +0530

    reverting to blank templates and ids

--- src/components/TemplatesAndIDs.jsx
+++ src/components/TemplatesAndIDs.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
 const TemplatesAndIDs = () => {
   return (
-    <div className="p-8 text-center text-gray-400 text-xl font-semibold">
-      Templates and IDs (coming soon)
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default TemplatesAndIDs; 
+export default TemplatesAndIDs
